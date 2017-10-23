cae8c77b520343ecb3ca54879473b5d2ab1a6493
renatasarmet <renatassmendes@hotmail.com>
2017-10-23 21:41:50 -0200

    Correção

--- GameJam/scripts/portinha_laranja.gd
+++ GameJam/scripts/portinha_laranja.gd
@@ -4,10 +4,9 @@ func _ready():
 	pass
 
 
-
-
 func _on_portinha_laranja_area_body_enter( body ):
 	if body.is_in_group(game.GRUPO_HEROI):
 		if game.chaveLaranja == true:
+			queue_free()
 			print("destrancando")
 	pass # replace with function body
